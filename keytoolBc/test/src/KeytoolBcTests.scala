8c996c5347bbab500e64f3ba795b3182215dfde9
Stylianos Anastasiou <stylianos@vaslabs.io>
2025-07-02 16:32:08 +0300

    some unit tests using keytool

---
+++ keytoolBc/test/src/KeytoolBcTests.scala
@@ -0,0 +1,64 @@
+package keytoolBc   
+
+import utest.*
+
+
+def getKeytoolOutput(args: String*): String = {
+  import scala.sys.process._
+  val cmd = Seq("keytool") ++ args
+  cmd.!!
+}
+
+def removeFile(filename: String): Unit = {
+  import java.io.File
+  val file = new File(filename)
+  if (file.exists()) {
+    if (!file.delete()) {
+      throw new RuntimeException(s"Failed to delete file: $filename")
+    }
+  }
+}
+
+
+object KeytoolBcTests extends TestSuite {
+  val tests = Tests {
+    test("Creates and Saves an Empty Keystore SUN") {
+        val filename = "keystore_sun.test"
+        val password = "password"
+        Keystore.saveEmptyKeystore(filename, password, "PKCS12", "SUN")
+        assert(getKeytoolOutput("-list", "-keystore", filename, "-storepass", password).contains("Your keystore contains 0 entries"))
+        removeFile(filename)
+    }
+    test("Creates and Saves an Empty Keystore BC") {
+        val filename = "keystore_bc.test"
+        val password = "password"
+        Keystore.saveEmptyKeystore(filename, password, "PKCS12", "BC")
+        assert(getKeytoolOutput("-list", "-keystore", filename, "-storepass", password).contains("Your keystore contains 0 entries"))
+        removeFile(filename)
+    }
+    test("Creates a keystore and adds a key pair SUN") {
+        val filename = "keystore_sun_with_key.test"
+        val password = "password"
+        val keyPair = RSAKeyGen.generateKeyPair(2048)
+        Keystore.saveEmptyKeystore(filename, password, "PKCS12", "SUN")
+        val ks = Keystore.loadKeystore(filename, password, "PKCS12", "SUN")
+        Keystore.addKeyPair(ks, "mykey", keyPair, password)
+        Keystore.saveKeystore(ks, filename, password)
+        val output = getKeytoolOutput("-list", "-keystore", filename, "-storepass", password)
+        assert(output.contains("Your keystore contains 1 entry") && output.contains("mykey"))
+        removeFile(filename)
+    }
+    test("Creates a keystore and adds a key pair BC") {
+        val filename = "keystore_bc_with_key.test"
+        val password = "password"
+        val keyPair = RSAKeyGen.generateKeyPair(2048)
+        Keystore.saveEmptyKeystore(filename, password, "PKCS12", "BC")
+        val ks = Keystore.loadKeystore(filename, password, "PKCS12", "BC")
+        Keystore.addKeyPair(ks, "mykey", keyPair, password)
+        Keystore.saveKeystore(ks, filename, password)
+        val output = getKeytoolOutput("-list", "-keystore", filename, "-storepass", password)
+        assert(output.contains("Your keystore contains 1 entry") && output.contains("mykey"))
+        removeFile(filename)
+    }
+  }
+}
